c07147f1841bcf5089dcc36eea09982828ebd01c
dswook <leewook94@naver.com>
2020-10-01 17:03:03 +0900

    add k-means code

--- src/DsProject/kmeans_data.py
+++ src/DsProject/kmeans_data.py
@@ -1,22 +1,33 @@
 # %%
 from data.datasets import train_model
 from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 import seaborn as sns
 
 # %%
 train = train_model()
 train.head()
 # %%
-kmeans = KMeans(n_clusters=150, init='k-means++', max_iter=300, random_state=0)
+kmeans = KMeans(n_clusters=20, init='k-means++', max_iter=300, random_state=0)
 kmeans.fit(train.iloc[:, 1:])
 # %%
-print(kmeans.labels_)
-# %%
 train['cluster'] = kmeans.labels_
-train.head()
+# %%
+pca = PCA(n_components=2)
+pca_transformed = pca.fit_transform(train.iloc[:, 1:])
+train['pca_x'] = pca_transformed[:, 0]
+train['pca_y'] = pca_transformed[:, 1]
+# %%
+fig, axes = plt.subplots(figsize=(12, 10))
+for i in range(kmeans.n_clusters):
+    sns.scatterplot(x='pca_x', y='pca_y', data=train[train['cluster'] == i], label=i)
+plt.xlabel('PCA 1')
+plt.ylabel('PCA 2')
+plt.legend()
+plt.title(f'{kmeans.n_clusters} cluster visualization by 2 pca components')
+plt.show()
+
 # %%
 train['cluster'].value_counts()
 # %%
